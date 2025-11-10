f4784868b204e8d2b1cec109adfd5d20dfa072a8
Plantain12345 <127324511+Plantain12345@users.noreply.github.com>
2025-11-10 22:27:37 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,6 @@
-// ===== config.js =====
 export const SUPABASE_URL = "https://gwvxchgvrdpiyfcujhzz.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3dnhjaGd2cmRwaXlmY3VqaHp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4MDY0NDgsImV4cCI6MjA3NDM4MjQ0OH0.xSbvdoHgiP7VTWK4ArR699HOCRafHvjdcWQSWccoDeE";
 
-// (Optional) Basic UI copy / constants
 export const APP = {
   brand: "Family Tree",
   codeLength: 6,
