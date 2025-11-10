59fa0195791d02a5038a3e26ee41a190de3e364c
Plantain12345 <127324511+Plantain12345@users.noreply.github.com>
2025-11-10 21:00:09 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // You can find these in: Supabase Dashboard > Settings > API
 
 export const SUPABASE_CONFIG = {
-  url: 'https://gwvxchgvrdpiyfcujhzz.supabase.co'
+  url: 'https://gwvxchgvrdpiyfcujhzz.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3dnhjaGd2cmRwaXlmY3VqaHp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4MDY0NDgsImV4cCI6MjA3NDM4MjQ0OH0.xSbvdoHgiP7VTWK4ArR699HOCRafHvjdcWQSWccoDeE'
 }
 
